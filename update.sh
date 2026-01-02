e643a3040d18890c60333f5fc7d18b74bdd00e2c
Raja Shankar Kolluru <raja@thekollurus.com>
2026-01-03 00:02:53 +0530

    change workflow file: Bump up version to 2.1.10

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ function grabVersion() {
 		 a= split($0,arr, ">")	;split (arr[2],arr1,"<"); print arr1[1]; start = 0;
 	}' pom.xml
 }
+read -p "Comments:" comment
 git add .
-git commit -m "Bump up version to $(grabVersion)"
+git commit -m "$comment: Bump up version to $(grabVersion)"
 git push origin main
